05485bf01f534f312181a014069487c356b7a70b
Sage <148996861+lungless-chest@users.noreply.github.com>
2024-02-22 10:11:54 -0700

    Update gigaScript.sh

--- gigaScript.sh
+++ gigaScript.sh
@@ -21,7 +21,8 @@
  sudo apt-get upgrade -y
  curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
  sudo apt-get install -y fzf
- echo 'export PATH=/home/$USER/.local/bin:"$PATH" >> ~/.bashrc'
+ echo "export PATH=/home/$USER/.local/bin:\$PATH" >> ~/.bashrc
+ echo 'eval "$(zoxide init --cmd cd bash)"' >> ~/.bashrc
  source ~/.bashrc
  exec bash
  
