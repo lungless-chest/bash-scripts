f7c7d8ed2992656227fabe5c3c4f3fb678a1922a
Sage <148996861+lungless-chest@users.noreply.github.com>
2024-02-23 14:52:25 -0700

    Update zshScript1.sh

--- zsh-config/zshScript1.sh
+++ zsh-config/zshScript1.sh
@@ -1,4 +1,4 @@
  sudo apt-get update
  sudo apt-get upgrade -y
  sudo apt-get install zsh zsh-autosuggestions zsh-syntax-highlighting git-core curl fonts-powerline
- sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)
+ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
